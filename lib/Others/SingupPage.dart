4463baf29571152343a018a0e4ddc84d5f8a6113
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-16 21:13:29 +0600

    singup commit

--- lib/Others/SingupPage.dart
+++ lib/Others/SingupPage.dart
@@ -11,94 +11,107 @@ class HomeActivity extends StatelessWidget{
 
 
     return Scaffold(
-      body:Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Sign up",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w800),),
-                SizedBox(height: 50,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "First Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.amber),
-
-                      )
-                  ),
+      body:Container(
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 50,),
+              Container(
+                height: 800,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(40)),
                 ),
-                SizedBox(height: 50,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Last Name",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.amber),
-
-                      )
-                  ),
-                ),
-                SizedBox(height: 50,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.amber),
-
-                      )
-                  ),
-                ),
-                SizedBox(height: 50,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.amber),
-
-                      )
-                  ),
-                ),
-                SizedBox(height: 50,),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: "Confirm Password",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5),
-                        borderSide: BorderSide(color: Colors.amber),
-
-                      )
-                  ),
-                ),
-                SizedBox(height: 50,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    ElevatedButton(onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));
-                    }, child: Text("Register",
-                    ),
-                      style: ButtonStyle(
-                        foregroundColor:MaterialStateProperty.all(Colors.white),
-                        backgroundColor: MaterialStateProperty.all(Colors.black),
-                        padding:MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40,vertical: 15),),
-                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(topRight:Radius.circular(15),bottomLeft:Radius.circular(15)),
-                        ),),
+                    SizedBox(height: 20,),
+                    Text("Sign up",style: TextStyle(fontSize: 30,fontWeight: FontWeight.w800),),
+                    SizedBox(height: 40,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "First Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(color: Colors.amber),
+
+                          )
                       ),
                     ),
-                  ],
-                )
+                    SizedBox(height: 40,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "Last Name",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(color: Colors.amber),
 
-              ],
-            ),
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 40,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(color: Colors.amber),
+
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 40,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(color: Colors.amber),
+
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 40,),
+                    TextField(
+                      decoration: InputDecoration(
+                          labelText: "Confirm Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                            borderSide: BorderSide(color: Colors.amber),
+
+                          )
+                      ),
+                    ),
+                    SizedBox(height: 50,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeActivity()));
+                        }, child: Text("Register",
+                        ),
+                          style: ButtonStyle(
+                            foregroundColor:MaterialStateProperty.all(Colors.white),
+                            backgroundColor: MaterialStateProperty.all(Colors.black),
+                            padding:MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 40,vertical: 15),),
+                            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(topRight:Radius.circular(15),bottomLeft:Radius.circular(15)),
+                            ),),
+                          ),
+                        ),
+                      ],
+                    )
+
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-      ),
+      )
     );
   }
 
