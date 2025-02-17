eb1b37ec8bc56e95a1d8a37020fcb09b742e182c
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-17 21:17:38 +0600

    cgpa commit

--- lib/Others/CGPACalculator.dart
+++ lib/Others/CGPACalculator.dart
@@ -13,8 +13,15 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   String myText =" ";
-  TextEditingController firstController=TextEditingController();
-  TextEditingController secondController=TextEditingController();
+  TextEditingController controller1=TextEditingController();
+  TextEditingController controller2=TextEditingController();
+  TextEditingController controller3=TextEditingController();
+  TextEditingController controller4=TextEditingController();
+  TextEditingController controller5=TextEditingController();
+  TextEditingController controller6=TextEditingController();
+  TextEditingController controller7=TextEditingController();
+  TextEditingController controller8=TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
