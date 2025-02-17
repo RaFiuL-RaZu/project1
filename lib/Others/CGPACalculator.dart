6618ebde14206a652b955adbb2e4f5da8510f642
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-17 19:14:57 +0600

    cgpa commit

--- lib/Others/CGPACalculator.dart
+++ lib/Others/CGPACalculator.dart
@@ -1,46 +1,22 @@
 
 
-import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget{
-   Calculator({Key? key}) : super(key: key);
-
-  String myText= "  ";
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<Calculator> createState() => _CalculatorState();
+}
 
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
         title: Text("CGPA Calculator"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 50,),
-            Text(myText),
-            SizedBox(height: 50,),
-            TextField(
-              onChanged: (value) {
-                myText = value;
-                log("=========== $myText =======");
-              },
-              decoration: InputDecoration(
-                labelText: "Write CGPA",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10))
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
-
 }
+
