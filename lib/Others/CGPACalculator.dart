90d7d2ef13465e8fc2f0309c163639f6bdab8187
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-17 19:40:40 +0600

    cgpa commit

--- lib/Others/CGPACalculator.dart
+++ lib/Others/CGPACalculator.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class Calculator extends StatefulWidget {
-  const Calculator({super.key});
+  const Calculator({Key? key}) : super(key: key);
 
   @override
   State<Calculator> createState() => _CalculatorState();
@@ -14,7 +14,27 @@ class _CalculatorState extends State<Calculator> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("CGPA Calculator"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text("Hello",style: TextStyle(fontSize: 28,fontWeight: FontWeight.w800),),
+              SizedBox(height: 50,),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "write cgpa",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:project1/Others/CGPACalculator.dart';
-import 'package:project1/Others/Loginpage.dart';
 
 main(){
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget{
   const MyApp({Key? key}) : super(key: key);
-
-
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculator());
+      home: Calculator(),
+    );
   }
 
+
 }
+
+
