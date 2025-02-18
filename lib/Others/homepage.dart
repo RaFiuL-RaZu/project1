4ec4a6dfcee83cd80c46ac8de2ada04693de12ff
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-18 16:08:32 +0600

    probasi commit

--- lib/Others/homepage.dart
+++ lib/Others/homepage.dart
@@ -17,8 +17,35 @@ class _HomePageDesihnState extends State<HomePageDesihn> {
         centerTitle: true,
         leading: Icon(Icons.menu),
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        title:Text("Ami Probashi",style: TextStyle(fontSize: 25,fontWeight:FontWeight.w500,color: Colors.green ),),
+        foregroundColor: Color(0xff1B7B77),
+        title:Text("Ami Probashi",style: TextStyle(fontSize: 20,fontWeight:FontWeight.w500),),
+        actions: [
+          Icon(Icons.mail),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Text("Your journey abroad",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w700),),
+                Icon(Icons.arrow_forward_ios_outlined),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+
+              )
+            ],
+          )
+        ],
       ),
 
     );
