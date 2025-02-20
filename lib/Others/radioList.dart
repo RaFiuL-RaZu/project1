88d225120dc63dec75e52a51b981255c70541f7a
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-20 19:58:04 +0600

    radio commit

--- lib/Others/radioList.dart
+++ lib/Others/radioList.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class RadioListDrop extends StatefulWidget {
@@ -11,12 +9,25 @@ class RadioListDrop extends StatefulWidget {
 
 class _RadioListDropState extends State<RadioListDrop> {
   @override
+  int selectedvalue = 2;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amber,
         centerTitle: true,
         title: Text("DropDown "),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RadioListTile(
+              value: 1,
+              groupValue: selectedvalue,
+              onChanged: (value) {
+              }),
+        ],
       ),
     );
   }
