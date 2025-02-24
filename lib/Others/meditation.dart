3b0d70bcf888a15e04e70891bdc3ad6d15aee6d5
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-24 19:34:04 +0600

    meditation commit

---
+++ lib/Others/meditation.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+
+class MeditaionApp extends StatefulWidget {
+  const MeditaionApp({Key? key}) : super(key: key);
+
+  @override
+  State<MeditaionApp> createState() => _MeditaionAppState();
+}
+
+class _MeditaionAppState extends State<MeditaionApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Discover"),
+        actions: [
+          Icon(Icons.search),
+        ],
+      ) ,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:project1/Others/CGPACalculator.dart';
 import 'package:project1/Others/Loginpage.dart';
 import 'package:project1/Others/Navigator.dart';
 import 'package:project1/Others/homepage.dart';
+import 'package:project1/Others/meditation.dart';
 import 'package:project1/Others/radioList.dart';
 
 main(){
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget{
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RadioListDrop(),
+      home: MeditaionApp(),
     );
   }
 
