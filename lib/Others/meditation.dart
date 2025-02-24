f967e42c132d9805e3009444afff2b8199e0b48d
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-24 20:21:12 +0600

    meditation commit

--- lib/Others/meditation.dart
+++ lib/Others/meditation.dart
@@ -12,18 +12,32 @@ class MeditaionApp extends StatefulWidget {
 class _MeditaionAppState extends State<MeditaionApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        backgroundColor: Colors.black,
-        foregroundColor:Colors.white,
-        title: Text("Discover",style: TextStyle(fontWeight: FontWeight.w800),),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(Icons.search,size: 30,),
-          ),
-        ],
-      ) ,
+    return DefaultTabController(
+        length: 8,
+        child: Scaffold(
+          appBar:AppBar(
+            backgroundColor: Colors.black,
+            foregroundColor:Colors.white,
+            title: Text("Discover",style: TextStyle(fontWeight: FontWeight.w800),),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.search,size: 30,),
+              ),
+            ],
+            bottom: TabBar(
+
+                tabs: [
+                  Tab(text: "Insomnia",),
+                  Tab(text: "Drag",),
+                  Tab(text: "Captul",),
+                  Tab(text: "Medicine",),
+                  Tab(text: "Insomnia",),
+                  Tab(text: "Insomnia",),
+                ]),
+          ) ,
+
+        )
     );
   }
 }
