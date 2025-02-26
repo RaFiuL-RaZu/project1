152eb8d64a309a676f587788153d1dd561815f6b
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-26 20:18:51 +0600

    meditation commit

--- lib/Others/meditation.dart
+++ lib/Others/meditation.dart
@@ -15,6 +15,8 @@ class _MeditaionAppState extends State<MeditaionApp> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
             height:250 ,
@@ -22,6 +24,26 @@ class _MeditaionAppState extends State<MeditaionApp> {
             decoration: BoxDecoration(
               color: Colors.deepPurple,
               borderRadius: BorderRadius.only(bottomLeft:Radius.circular(25),bottomRight: Radius.circular(25) ),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.menu),
+                      Spacer(),
+                      CircleAvatar(
+                       child: ClipRRect(
+                         borderRadius: BorderRadius.circular(20),
+                         child:Image.network("https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"),
+                       ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           )
         ],
