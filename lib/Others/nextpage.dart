e6a327035a5ae6fd62be3848ba2d37a8df90fd24
RaFiuL-RaZu <rafi102621@gmail.com>
2025-02-27 20:54:10 +0600

    meditation commit

--- lib/Others/nextpage.dart
+++ lib/Others/nextpage.dart
@@ -24,8 +24,9 @@ class _NextpageDesignState extends State<NextpageDesign> {
                 child: Image.network("https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg"),
               ),
             ),
-            Text("Dr.RaFiuL Razu"),
+            Text("Dr.RaFiuL"),
             Chip(
+
                 label: Text("Cardio speacalist"),)
 
           ],
