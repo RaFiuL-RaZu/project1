f7bd13b7bf64dd55835d973151d5e7108304dec8
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-02 10:15:20 +0600

    note commit

--- lib/Others/NoteApp.dart
+++ lib/Others/NoteApp.dart
@@ -16,25 +16,45 @@ class Noteapp extends StatefulWidget {
        appBar: AppBar(
          centerTitle:true,
          title: Text("Note App"),
-         backgroundColor: Colors.black,
          foregroundColor: Colors.white,
        ),
        body: Center(
          child: Column(
            children: [
-
              SizedBox(height: 20,),
              Padding(
-               padding: const EdgeInsets.all(8.0),
+               padding: const EdgeInsets.all(15.0),
                child: TextField(
-                 decoration: InputDecoration(
-                   border: OutlineInputBorder(
-                     borderRadius: BorderRadius.circular(10),
-                     borderSide: BorderSide(color: Colors.white),
-                   )
+                 textAlign: TextAlign.center,
+                   decoration: InputDecoration(
+                     hintText: "Search",
+                     contentPadding: EdgeInsets.symmetric(horizontal: 15,vertical: 0),
+                     fillColor: Color(0xfff272727),
+                     filled: true,
+                     border: OutlineInputBorder(
+                       borderRadius: BorderRadius.circular(10),
+                       borderSide: BorderSide.none,
+                     )
+                   ),
+                 ),
+             ),
+             SizedBox(height: 20,),
+             Card(
+               color: Colors.white,
+               child: SizedBox(
+                 height: 200,
+                 width: MediaQuery.sizeOf(context).width/3,
+                 child: Column(
+                   crossAxisAlignment: CrossAxisAlignment.center,
+                   children: [
+                     Text("${DateTime.now().toString().substring(0,16)}",style:TextStyle(color: Colors.grey),),
+                     SizedBox(height: 10,),
+                     Text("Song for the old onece",style: TextStyle(color: Colors.black,fontSize: 18,fontWeight: FontWeight.w600),),
+                   ],
                  ),
                ),
-             ),
+             )
+        
            ],
          ),
        ),
