c6be364d4ad799dc4204d2ed1fe33ef0663754a5
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-03 10:00:29 +0600

    extra commit

--- lib/Extras/PracticeNoteApp.dart
+++ lib/Extras/PracticeNoteApp.dart
@@ -32,15 +32,37 @@ class _PNoteAppState extends State<PNoteApp> {
               child: TextField(
                 decoration: InputDecoration(
                   contentPadding: EdgeInsets.symmetric(horizontal: 15,vertical: 0),
-                  fillColor: Colors.grey.shade600,
+                  fillColor: Color(0xf404040),
                   filled: true,
                   prefixIcon: Icon(Icons.search),
                   hintText: "Search notes....",
                   hintStyle:TextStyle(letterSpacing:1),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide.none,
-                  )
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.grey),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.teal.shade700,
+              child: SizedBox(
+                height: 100,
+                width: double.infinity,
+                child:Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Bucket List",style:TextStyle(fontSize: 22,fontWeight: FontWeight.w800,color: Colors.black),),
+                      Text("1. Travel to japan",style:TextStyle(fontSize: 16,fontWeight: FontWeight.w200,color: Colors.grey.shade400),),
+                      Text("2. Learn to play guitter",style:TextStyle(fontSize: 16,fontWeight: FontWeight.w200,color: Colors.grey.shade400),),
+                    ],
+                  ),
                 ),
               ),
             )
