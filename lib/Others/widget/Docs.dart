84dc13f5341ed9d3189e162c426567e535288557
RaFiuL-RaZu <rafi102621@gmail.com>
2025-03-04 16:28:58 +0600

    Containerimg commit

--- lib/Others/widget/Docs.dart
+++ lib/Others/widget/Docs.dart
@@ -17,11 +17,32 @@ class _DocsFileState extends State<DocsFile> {
         children: [
           Opacity(opacity: 0.85,
           child: Container(
-            child: Image.asset("assets/doctor.png"),
-            margin:EdgeInsets.only(left: 20,top: 20),
-            width: 200,
-          ),)
 
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              color: Colors.amber,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              image: DecorationImage(
+                  image:ExactAssetImage("assets/doctor.png"),
+              fit: BoxFit.contain),
+              boxShadow:[ BoxShadow(
+                spreadRadius: 0.10,
+                blurRadius: 10,
+                color: Colors.white,
+                offset: Offset(5, 5),
+              )
+              ]
+            ),
+          ),),
+
+          Column(
+            children: [
+              Text("Razu"),
+              TextButton(onPressed: (){}, child: Text("Submit"))
+            ],
+          )
         ],
       ),
     );
